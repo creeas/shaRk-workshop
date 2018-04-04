813685ca7e252ef4d79a69fdd528e59479f4fc88
creeas <cmull@sfu.ca>
2018-04-04 13:19:10 -0700

    change filepath and basic model example

--- Code/brain.code.R
+++ Code/brain.code.R
@@ -1,6 +1,6 @@
 #workshop code
 #load in the dataset
-brain <- read.csv("Workshop.data.csv", header = T)
+brain <- read.csv("Data/brain.data.csv", header = T)
 
 #plot all the data
 plot(brain)
@@ -10,5 +10,5 @@ plot(log10(brain$brain.mass)~log10(brain$body.mass), pch=21, bg=as.factor(brain$
 plot(log10(brain$brain.mass)~log10(brain$body.mass), pch=21, bg=as.factor(brain$habitat))
 
 #basic models
-mod <- glm(log10(Brain.mass.wolf)~log10(body.mass):habitat.new, data = brain)
+mod <- glm(log10(brain.mass)~log10(body.mass) + habitat, data = brain)
 summary(mod)
